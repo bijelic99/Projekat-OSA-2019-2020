26b54dcc413ab6ced558d68cb7d9403ef991947a
Dejan Bijelic <bijelic.99.dejan@hotmail.rs>
2020-04-09 22:15:53 +0200

    Initial commit

---
+++ src/main/java/com/ftn/osa/projekat_osa/ProjekatOsa20192020Application.java
@@ -0,0 +1,13 @@
+package com.ftn.osa.projekat_osa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjekatOsa20192020Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjekatOsa20192020Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ftn/osa/projekat_osa/ProjekatOsa20192020ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ftn.osa.projekat_osa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjekatOsa20192020ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
