b36d128fcee84756ca6b68ef87aa9a0db389fb23
Dejan Bijelic <bijelic.99.dejan@hotmail.rs>
2020-05-18 18:39:21 +0200

    Pravilno importovan log4j2 u projekat, preostaje samo napisati konfiguraciju za njega

--- src/main/java/com/ftn/osa/projekat_osa/logging/UserLogger.java
+++ src/main/java/com/ftn/osa/projekat_osa/logging/UserLogger.java
@@ -1,20 +1,22 @@
 package com.ftn.osa.projekat_osa.logging;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class UserLogger {
 
+    Logger logger = LoggerFactory.getLogger(UserLogger.class);
+
     @AfterReturning("execution(* com..projekat_osa.controller.RegisterController.*(..))")
     public void logAfterNewUserRegister(JoinPoint joinPoint)
     {
-        Logger logger = LogManager.getLogger(this.getClass().getName());
+
         logger.info("User registered");
         logger.error("dada");
 
