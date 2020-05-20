42c64ee2718a165680fee4fd8027114d5b1250ca
Dejan Bijelic <bijelic.99.dejan@hotmail.rs>
2020-05-20 12:31:24 +0200

    izmene na FolderDTO zbog inf loop errora

--- src/main/java/com/ftn/osa/projekat_osa/android_dto/FolderDTO.java
+++ src/main/java/com/ftn/osa/projekat_osa/android_dto/FolderDTO.java
@@ -10,8 +10,8 @@ import java.util.stream.Collectors;
 public class FolderDTO extends DtoObject<Folder> {
     private Long id;
     private String name;
-    private FolderDTO parentFolder;
-    private Set<FolderDTO> folders;
+    private Long parentFolder;
+    private Set<Long> folders;
     private Set<MessageDTO> messages;
 
     public FolderDTO(){
@@ -19,7 +19,7 @@ public class FolderDTO extends DtoObject<Folder> {
         messages = new HashSet<>();
     }
 
-    public FolderDTO(Long id, String name, FolderDTO parentFolder, Set<FolderDTO> folders, Set<MessageDTO> messages) {
+    public FolderDTO(Long id, String name, Long parentFolder, Set<Long> folders, Set<MessageDTO> messages) {
         this.id = id;
         this.name = name;
         this.parentFolder = parentFolder;
@@ -28,13 +28,14 @@ public class FolderDTO extends DtoObject<Folder> {
     }
 
     public FolderDTO(Folder entity) {
+        this();
         this.id = entity.getId() != null ? entity.getId() : 0;
         this.name = entity.getName();
-        this.parentFolder = entity.getParentFolder() != null ? new FolderDTO(entity.getParentFolder()) : null;
-        this.folders = entity.getFolders().size() > 0 ?
-                entity.getFolders().stream().map(folder -> new FolderDTO(folder)).collect(Collectors.toSet()) : null;
-        this.messages = entity.getMessages().size() > 0 ?
-                entity.getMessages().stream().map(message -> new MessageDTO(message)).collect(Collectors.toSet()) : null;
+        this.parentFolder = entity.getParentFolder() != null ? entity.getParentFolder().getId() : null;
+        if(entity.getFolders().size() > 0)
+            this.folders = entity.getFolders().stream().map(folder -> folder.getId()).collect(Collectors.toSet());
+        if(entity.getMessages().size() > 0)
+            this.messages = entity.getMessages().stream().map(message -> new MessageDTO(message)).collect(Collectors.toSet());
     }
 
     public Long getId() {
@@ -54,11 +55,11 @@ public class FolderDTO extends DtoObject<Folder> {
     }
 
 
-    public Set<FolderDTO> getFolders() {
+    public Set<Long> getFolders() {
         return folders;
     }
 
-    public void setFolders(Set<FolderDTO> folders) {
+    public void setFolders(Set<Long> folders) {
         this.folders = folders;
     }
 
@@ -70,19 +71,33 @@ public class FolderDTO extends DtoObject<Folder> {
         this.messages = messages;
     }
 
-    @JsonIgnore
-    public FolderDTO getParentFolder() {
+
+    public Long getParentFolder() {
         return parentFolder;
     }
 
-    public void setParentFolder(FolderDTO parentFolder) {
+    public void setParentFolder(Long parentFolder) {
         this.parentFolder = parentFolder;
     }
 
+    /**
+     * <p>
+     * Vraca JPA objekat od FolderDTO.
+     * Zbog ispravke za infinite loop error, ne vraca dobro parentFolder i listu foldera koju sadrzi.
+     * Tj. na njihovom mestu vraca JPA objekte sa samo id popunjenim, ostale vrednosti su null i ukoliko su nam potrebne treba ih preuzeti iz baze.
+     * </p>
+     * <p>
+     * Function returns jpa object representation of FolderDTO.
+     * However, due to fix for infinite loop error, it does not return parent folder or folders correctly.
+     * It returns new JPA object for each of them with only their id set, rest of the values are null.
+     * It is up to the user to find these values in the db and set them.
+     * </p>
+     * @return new JPA object that needs some editing
+     */
     @Override
     public Folder getJpaEntity() {
-        return new Folder(this.getId(), this.getName(), this.getParentFolder().getJpaEntity(),
+        return new Folder(this.getId(), this.getName(), new Folder(this.getParentFolder(), null, null, null, null),
                 this.getMessages().stream().map(message -> message.getJpaEntity()).collect(Collectors.toSet()),
-                this.getFolders().stream().map(folder -> folder.getJpaEntity()).collect(Collectors.toSet()));
+                this.getFolders().stream().map(folder ->  new Folder(folder, null, null, null, null)).collect(Collectors.toSet()));
     }
 }
