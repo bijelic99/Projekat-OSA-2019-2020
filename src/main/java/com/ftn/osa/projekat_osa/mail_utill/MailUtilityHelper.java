bea6d22390509fe770763b67c1ccb3b590d68bf5
Dejan Bijelic <bijelic.99.dejan@hotmail.rs>
2020-05-27 15:58:33 +0200

    Nisam siguran

--- src/main/java/com/ftn/osa/projekat_osa/mail_utill/MailUtilityHelper.java
+++ src/main/java/com/ftn/osa/projekat_osa/mail_utill/MailUtilityHelper.java
@@ -67,7 +67,7 @@ public interface MailUtilityHelper {
                     })
                     .collect(Collectors.toSet()));
         }
-
+        if(folder.isOpen()) folder.close();
         return jpaFolder;
     }
 
