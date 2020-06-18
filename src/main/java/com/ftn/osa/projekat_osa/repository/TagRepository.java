53fc1ed77f8212cccd81d6cb7bc8fa0d71f95893
Svetozar99 <svetozarb999@gmail.com>
2020-06-18 23:51:47 +0000

    manje izmjene kod tagova

--- src/main/java/com/ftn/osa/projekat_osa/repository/TagRepository.java
+++ src/main/java/com/ftn/osa/projekat_osa/repository/TagRepository.java
@@ -1,7 +1,14 @@
 package com.ftn.osa.projekat_osa.repository;
 
+import com.ftn.osa.projekat_osa.model.Contact;
 import com.ftn.osa.projekat_osa.model.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Set;
 
 public interface TagRepository extends JpaRepository<Tag, Long> {
+    @Query("Select u.userTags from User u where u.id = :id")
+    Set<Tag> getUsersTags(@Param("id") Long id);
 }
