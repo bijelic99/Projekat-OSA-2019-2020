b56d0c3f1c40c4af0b32332a170c1950b3a5a6ab
Svetozar99 <svetozarb999@gmail.com>
2020-06-18 23:16:17 +0000

    uradjeno dodavanje tagova za korisnika

--- src/main/java/com/ftn/osa/projekat_osa/controller/UserController.java
+++ src/main/java/com/ftn/osa/projekat_osa/controller/UserController.java
@@ -1,14 +1,17 @@
 package com.ftn.osa.projekat_osa.controller;
 
 import com.ftn.osa.projekat_osa.android_dto.ContactDTO;
+import com.ftn.osa.projekat_osa.android_dto.TagDTO;
 import com.ftn.osa.projekat_osa.android_dto.UserDTO;
 import com.ftn.osa.projekat_osa.exceptions.ResourceNotFoundException;
 import com.ftn.osa.projekat_osa.exceptions.WrongPasswordException;
 import com.ftn.osa.projekat_osa.model.Contact;
+import com.ftn.osa.projekat_osa.model.Tag;
 import com.ftn.osa.projekat_osa.model.User;
 import com.ftn.osa.projekat_osa.service.ContactService;
 import com.ftn.osa.projekat_osa.service.UserService;
 import com.ftn.osa.projekat_osa.service.serviceInterface.ContactServiceInterface;
+import com.ftn.osa.projekat_osa.service.serviceInterface.TagServiceInterface;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -28,6 +31,9 @@ public class UserController {
 
     @Autowired
     private ContactServiceInterface contactService;
+
+    @Autowired
+    private TagServiceInterface tagService;
 
     @PutMapping(value = "/{id}/changePassword")
     public ResponseEntity<Object> changePassword(@PathVariable(value = "id") Long id, @RequestBody Map<String, String> data) {
@@ -69,4 +75,16 @@ public class UserController {
         Contact contact1 = contactService.addUserContact(id, contact.getJpaEntity());
         return new ResponseEntity<>(new ContactDTO(contact1), HttpStatus.OK);
     }
+
+    @GetMapping("{id}/tags")
+    public ResponseEntity<Set<TagDTO>> getUserTags(@PathVariable(value = "id") Long id){
+        Set<Tag> tags = tagService.getUsersTags(id);
+        return new ResponseEntity<>(tags.stream().map(TagDTO::new).collect(Collectors.toSet()), HttpStatus.OK);
+    }
+    @PostMapping(value = "/{id}/tags", consumes = "application/json")
+    public ResponseEntity<TagDTO> addUserTag(@PathVariable(value = "id") Long id, @RequestBody TagDTO tag){
+        Tag tag1 = tagService.addUserTag(id, tag.getJpaEntity());
+        return  new ResponseEntity<>(new TagDTO(tag1), HttpStatus.OK);
+    }
+
 }
